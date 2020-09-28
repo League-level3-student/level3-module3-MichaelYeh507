83a08fa57866a3eeaad913705cd77eeffa9976db
MichaelYeh507 <54426940+MichaelYeh507@users.noreply.github.com>
2020-09-28 15:26:45 -0700

    TextFunkier

--- src/_00_Text_Funkifier/MixedCapsString.java
+++ src/_00_Text_Funkifier/MixedCapsString.java
@@ -8,7 +8,15 @@ public class MixedCapsString extends SpecialString{
 
 	@Override
 	public String funkifyText(String s) {
-		
+		String mixedCaps = "";
+		for(int i = 0; i<s.length(); i++) {
+			if(i%2==0) {
+				mixedCaps = mixedCaps + Character.toLowerCase(s.charAt(i));
+			} else if(i%2==1) {
+				mixedCaps = mixedCaps + Character.toUpperCase(s.charAt(i));
+			}
+
+		}
 		return "";
 	}
 
